4e0d0de20bc2f0b28465837d08421e98bde6da7b
2301003 <2301003@systex.com.tw>
2026-01-16 17:23:11 +0800

    新增儲存TrainingDayLog功能

--- app/db.ts
+++ app/db.ts
@@ -1,5 +1,5 @@
 import * as SQLite from "expo-sqlite";
-import type { ProgramExercise, TrainingDayLog } from "./types";
+import type { ExerciseLog, ProgramExercise, SetLog, TrainingDayLog } from "./types";
 
 const db = SQLite.openDatabaseSync("gymlogger.db");
 
@@ -17,7 +17,8 @@ export const initDb = () => {
     CREATE TABLE IF NOT EXISTS program (
       id TEXT PRIMARY KEY NOT NULL,
       name TEXT NOT NULL,
-      create_time INTEGER NOT NULL
+      create_time INTEGER NOT NULL,
+      update_time INTEGER NOT NULL
     );
     CREATE TABLE IF NOT EXISTS program_exercise (
       id TEXT PRIMARY KEY NOT NULL,
@@ -25,21 +26,24 @@ export const initDb = () => {
       name TEXT NOT NULL,
       unit TEXT,
       image_uri TEXT,
-      create_time INTEGER NOT NULL
+      create_time INTEGER NOT NULL,
+      update_time INTEGER NOT NULL
     );
     CREATE TABLE IF NOT EXISTS program_set (
       id TEXT PRIMARY KEY NOT NULL,
       exercise_id TEXT,
       reps INTEGER,
       weight INTEGER,
-      create_time INTEGER NOT NULL
+      create_time INTEGER NOT NULL,
+      update_time INTEGER NOT NULL
     );
     CREATE TABLE IF NOT EXISTS set_log (
       id TEXT PRIMARY KEY NOT NULL,
       exercise_id TEXT,
       reps INTEGER,
       weight INTEGER,
-      create_time INTEGER NOT NULL
+      create_time INTEGER NOT NULL,
+      update_time INTEGER NOT NULL
     );
     CREATE TABLE IF NOT EXISTS exercise_log (
       id TEXT PRIMARY KEY NOT NULL,
@@ -48,24 +52,27 @@ export const initDb = () => {
       name TEXT NOT NULL,
       unit TEXT,
       imageUri TEXT;
-      create_time INTEGER NOT NULL
+      create_time INTEGER NOT NULL,
+      update_time INTEGER NOT NULL
     );
     CREATE TABLE IF NOT EXISTS training_day_log (
       id TEXT PRIMARY KEY NOT NULL,
       date INTEGER NOT NULL,
-      create_time INTEGER NOT NULL
+      create_time INTEGER NOT NULL,
+      update_time INTEGER NOT NULL
     );
   `);
 };
 
 export const listPrograms = (): ProgramRow[] =>
-  db.getAllSync("SELECT id, name FROM programs ORDER BY created_at DESC") as ProgramRow[];
+  db.getAllSync("SELECT id, name FROM programs ORDER BY create_time DESC") as ProgramRow[];
 
 export const createProgram = (name: string): ProgramRow => {
   const id = createId("program");
-  db.runSync("INSERT INTO programs (id, name, created_at) VALUES (?, ?, ?)", [
+  db.runSync("INSERT INTO programs (id, name, create_time, update_time) VALUES (?, ?, ?)", [
     id,
     name,
+    Date.now(),
     Date.now(),
   ]);
   return { id, name };
@@ -73,7 +80,7 @@ export const createProgram = (name: string): ProgramRow => {
 
 export const getProgramExercisesByProgramId = (programId: string): ProgramExercise[] => {
   const exercises = db.getAllSync(
-    "SELECT id, programId, name, unit, image_uri FROM exercises WHERE program_id = ? ORDER BY created_at DESC",
+    "SELECT id, programId, name, unit, image_uri FROM exercises WHERE program_id = ? ORDER BY create_time DESC",
     [programId]
   ) as {
     id: string;
@@ -127,18 +134,19 @@ export const saveExercise = (programId: string, value: ProgramExercise): string 
 
   if (existing) {
     db.runSync(
-      "UPDATE exercises SET name = ?, unit = ?, image_uri = ? WHERE id = ?",
-      [value.name, value.unit, value.imageUri ?? null, exerciseId]
+      "UPDATE exercises SET name = ?, unit = ?, image_uri = ?, update_time WHERE id = ?",
+      [value.name, value.unit, value.imageUri ?? null, Date.now(), exerciseId]
     );
   } else {
     db.runSync(
-      "INSERT INTO exercises (id, program_id, name, unit, image_uri, created_at) VALUES (?, ?, ?, ?, ?, ?)",
+      "INSERT INTO exercises (id, program_id, name, unit, image_uri, create_time, update_time) VALUES (?, ?, ?, ?, ?, ?, ?)",
       [
         exerciseId,
         programId,
         value.name,
         value.unit,
         value.imageUri ?? null,
+        Date.now(),
         Date.now(),
       ]
     );
@@ -161,27 +169,88 @@ export const deleteExercise = (exerciseId: string) => {
   db.runSync("DELETE FROM exercises WHERE id = ?", [exerciseId]);
 };
 
-export const saveTraningDayLog = (value: TrainingDayLog) => {
-  // const trainingDayLogId = value.id;
-  // const exists = db.getFirstSync("SELECT id FROM training_day_log WHERE id = ?", [
-  //   trainingDayLogId,
-  // ]) as { id: string } | undefined;
+export const saveExerciseLogs = (values: ExerciseLog[]) => {
+  values.forEach((value) => {
+    db.runSync(
+      `INSERT INTO exercise_log (id, exercise_id, training_log_id, name, unit, image_uri)
+      VALUES (?, ?, ?, ?, ?, ?)
+      ON CONFLICT(id) DO UPDATE SET
+        exercise_id = excluded.exercise_id,
+        training_log_id = excluded.training_log_id,
+        name = excluded.name,
+        unit = excluded.unit,
+        image_uri = excluded.image_uri`,
+      [
+        value.id,
+        value.exerciseId ?? null,
+        value.trainingLogId ?? null,
+        value.name,
+        value.unit,
+        value.imageUri ?? null,
+      ]
+    );
+  });
+};
 
-  // if(exists) {
-  //       db.runSync(
-  //     "UPDATE exercises SET name = ?, unit = ?, image_uri = ? WHERE id = ?",
-  //     [value.name, value.unit, value.imageUri ?? null, exerciseId]
-  //   );
-  // } else {
-  //   db.runSync(
-  //     "INSERT INTO exercises (id, program_id, name, unit, image_uri, created_at) VALUES (?, ?, ?, ?, ?, ?)",
-  //     [
-  //       exerciseId,
-  //       programId,
-  //       value.name,
-  //       value.unit,
-  //       value.imageUri ?? null,
-  //       Date.now(),
-  //     ]
-  // }
+export const saveSetLogs = (values: SetLog[]) => {
+  values.forEach((value) => {
+    db.runSync(
+      `INSERT INTO set_log (id, exercise_log_id, reps, weight)
+      VALUES (?, ?, ?, ?)
+      ON CONFLICT(id) DO UPDATE SET
+        id = excluded.id,
+        exercise_log_id = excluded.exercise_log_id,
+        reps = excluded.reps,
+        weight = excluded.weight`,
+      [
+        value.id,
+        value.exerciseLogId ?? null,
+        value.reps ?? null,
+        value.weight ?? null,
+      ]
+    );
+  });
+};
+
+export const saveExerciseLogsWithSets = (
+  exerciseLogs: ExerciseLog[]
+) => {
+  saveExerciseLogs(exerciseLogs);
+  exerciseLogs.map((exerciseLog) => {
+    {
+      saveSetLogs(exerciseLog.sets);
+    }
+  })
+};
+
+export const saveTraningDayLog = (value: TrainingDayLog) => {
+  const trainingDayLogId = value.id;
+  const exists = db.getFirstSync("SELECT id FROM training_day_log WHERE id = ?", [
+    trainingDayLogId,
+  ]) as { id: string } | undefined;
+
+  db.execSync("BEGIN");
+  try {
+    if(exists) {
+          db.runSync(
+        "UPDATE training_day_log SET date = ? update_time = ? WHERE id = ?",
+        [value.date.getDate(), Date.now()]
+      );
+      saveExerciseLogsWithSets(value.exerciseLogs);
+    } else {
+      db.runSync(
+        "INSERT INTO training_day_log (id, date, create_time, update_time) VALUES (?, ?, ?, ?)",
+        [
+          value.id,
+          value.date.getDate(),
+          Date.now(),
+          Date.now()
+        ]);
+      saveExerciseLogsWithSets(value.exerciseLogs);
+    }
+    db.execSync("COMMIT");
+  } catch (error) {
+    db.execSync("ROLLBACK");
+    throw error;
+  }
 }
